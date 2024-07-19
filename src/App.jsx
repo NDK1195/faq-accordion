cbe96ead311f4c12e5785346545e66f6473448f8
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-07-19 19:46:23 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <div className='mx-6 bg-white z-10 rounded-lg md:rounded-2xl min-w-[327px] max-w-[600px] shadow-custom-shadow p-6 md:p-10 flex flex-col gap-6 md:gap-8'>
           <div className='flex items-center gap-6'>
             <img src={iconStar} alt='icon star' className='size-6 md:size-10' />
-            <h1 className='text-dark-purple font-bold text-[32px]md:text-[56px]'>
+            <h1 className='text-dark-purple font-bold text-[32px] md:text-[56px]'>
               FAQs
             </h1>
           </div>
